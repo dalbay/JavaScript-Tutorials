fa485469182a19e93bd25dca2faad15a840f2f1c
dalbay <fadalbay@gmail.com>
2019-09-20 10:22:47 -0400

    object literals

---
+++ JAVASCRIPT LANGUAGE FUNDAMENTALS/app12-template_literals.js
@@ -0,0 +1,48 @@
+// Template Literals
+/*
+A lot of times, you will be inserting HTML from JavaScript; when you fetch something through Ajax and you want to put in something in an unordered list or a table with dynamic data, you going to do that with JavaScript.
+*/
+const name = 'John';
+const age = 31;
+const job = 'Web Developer';
+const city = 'Miami';
+let html;
+
+// Without template strings (es5)
+html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + ' </li><li>Job: '+ job+ ' </li><li>City: '+ city +' </li></ul>';
+
+html = '<ul>' +
+       '<li>Name: ' + name + '</li>' +
+       '<li>Age: ' + age + '</li>' +
+       '<li>Job: ' + job + '</li>' +
+       '<li>City: ' + city + '</li>' +
+       '</ul>';
+
+function hello(){
+  return 'hello';
+}
+
+// With template strings (es6)
+html = `
+  <ul>
+    <li>Name: ${name}</li>
+    <li>Age: ${age}</li>
+    <li>Job: ${job}</li>
+    <li>City: ${city}</li>
+    <li>${2 + 2}</li>
+    <li>${hello()}</li>
+    <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+  </ul>
+`;
+
+document.body.innerHTML = html;
+/*
+Output: 
+•	Name: John
+•	Age: 31
+•	Job: Web Developer
+•	City: Miami
+•	4
+•	hello
+•	Over 30
+*/
